65ceba4bf8a8098797ec30ab877c9be22f022e46
airrobo <andrew.m.yan@gmail.com>
2020-09-12 21:27:19 -0400

    most recent

--- index.js
+++ index.js
@@ -2,39 +2,39 @@
 
  /////Math Stuff////////
 
- export function stopCalculations {
+// function stopMath (s) {
 
-    var sumX = store_X.reduce((sumX, val) => (sumX += val));
-    var sumY = store_Y.reduce((sumY, val) => (sumY +=val))
+//     var sumX = store_X.reduce((sumX, val) => (sumX += val));
+//     var sumY = store_Y.reduce((sumY, val) => (sumY +=val))
 
-    var averageX = sumX/store_X.length;
-    var averageY = sumY/store_Y.length;
+//     var averageX = sumX/store_X.length;
+//     var averageY = sumY/store_Y.length;
     
-    //const xSort = store_X.sort();
+//     //const xSort = store_X.sort();
 
-    // const arrSort = arr.sort();
-    // const mid = Math.ceil(len / 2);
+//     // const arrSort = arr.sort();
+//     // const mid = Math.ceil(len / 2);
 
-    // const median =
-    // len % 2 == 0 ? (arrSort[mid] + arrSort[mid - 1]) / 2 : arrSort[mid - 1];
-    //const ySort = store_Y.sort();
+//     // const median =
+//     // len % 2 == 0 ? (arrSort[mid] + arrSort[mid - 1]) / 2 : arrSort[mid - 1];
+//     //const ySort = store_Y.sort();
 
     
-    // var sumX;
-    // var sumY;
-    // for(i in store_X){
-    //     sumX +=store_X[i];
-    // }
-    // for(i in store_Y){
-    //     sumY +=store_Y[i];
-    // }
+//     // var sumX;
+//     // var sumY;
+//     // for(i in store_X){
+//     //     sumX +=store_X[i];
+//     // }
+//     // for(i in store_Y){
+//     //     sumY +=store_Y[i];
+//     // }
     
 
-    console.log('stopped');
-    console.log(averageX)
-    console.log(averageY)
+//     console.log('stopped');
+//     console.log(averageX)
+//     console.log(averageY)
 
- }
+//  };
            
 
 //GazeCloudAPI.StartEyeTracking();
