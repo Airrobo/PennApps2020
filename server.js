cfa94c19450970925a25669872ed7b54c1221b03
airrobo <andrew.m.yan@gmail.com>
2020-09-13 02:36:11 -0400

    ready for css styling

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
+//setting middleware
+app.use(express.static('static'));
+
 app.get('/', (req, res) => {
-    res.sendFile('/Users/andrew/Documents/GazeCloud/index.html');
+    res.sendFile('/Users/andrew/Documents/GazeCloud/public/index.html');
 });
 
 app.listen(3000, () => console.log(' listening on port 3000!'));
